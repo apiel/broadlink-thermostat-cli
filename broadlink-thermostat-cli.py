0867f005cb93afb671e0db406f03f5969e6b574b
Alexandre Piel <alexandre.piel@gmail.com>
2019-01-06 21:26:31 +0100

    yeap

--- broadlink-thermostat-cli.py
+++ broadlink-thermostat-cli.py
@@ -13,8 +13,7 @@ def main():
     print "--mode2=auto|manual"
     print "--temp=22"
     print "--power=on|off"
-    print "--schedule='[{\"start_hour\":6,\"temp\":23.0,\"start_minute\":0}]'"
-    # ./broadlink-thermostat-cli.py --schedule='[{"start_hour":6,"temp":23.0,"start_minute":0},{"start_hour":23,"temp":15.0,"start_minute":0}]'
+    print "--schedule='[[{\"start_hour\":6,\"temp\":23.0,\"start_minute\":0},...],[...]]'" # ./broadlink-thermostat-cli.py --schedule='[[{"start_hour":6,"temp":22.5,"start_minute":0},{"start_hour":23,"temp":15.0,"start_minute":0},{"start_hour":23,"temp":15.0,"start_minute":0},{"start_hour":23,"temp":15.0,"start_minute":0},{"start_hour":23,"temp":15.0,"start_minute":0},{"start_hour":23,"temp":15.0,"start_minute":0}],[{"start_hour":6,"temp":22.5,"start_minute":0},{"start_hour":23,"temp":15.0,"start_minute":0}]]'
     args = [arg.split('=', 1) for arg in sys.argv[1:]]
 
     devices = broadlink.discover(timeout=5)
@@ -33,11 +32,12 @@ def main():
                 elif arg[0] == "--schedule":
                     schedules=json.loads(arg[1])
                     if len(schedules) != 2:
-                        print "schedule should contain 2 value, start and end."
+                        print "schedule should contain 2 array, weekday and weekend."
                     else:
-                        schedules = [schedules[0], schedules[1], schedules[1], schedules[1], schedules[1], schedules[1]] # the thermostat is expecting 6 values but 2 are enough
-                        device.set_schedule(schedules, schedules)
+                        device.set_schedule(schedules[0], schedules[1])
             print device.type
+            data = device.get_full_status()
+            print json.dumps(data)
         else:
             print "error: could not auth"
     print "done"
